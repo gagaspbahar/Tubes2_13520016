47a1a47b791c915847969ef4002c15fc309593dd
gagaspbahar <gagaspbahar@gmail.com>
2022-03-17 23:28:26 +0700

    merge

--- FolderCrawler/Form1.Designer.cs
+++ FolderCrawler/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.header = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // header
+            // 
+            this.header.AutoSize = true;
+            this.header.Font = new System.Drawing.Font("Open Sans", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.header.Location = new System.Drawing.Point(258, 9);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(229, 39);
+            this.header.TabIndex = 0;
+            this.header.Text = "Folder Crawler";
+            this.header.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // input
+            // 
+            this.input.AutoSize = true;
+            this.input.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(47, 53);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(69, 28);
+            this.input.TabIndex = 1;
+            this.input.Text = "Input";
+            this.input.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(451, 53);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(88, 28);
+            this.output.TabIndex = 2;
+            this.output.Text = "Output";
+            this.output.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.header);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label header;
+        private System.Windows.Forms.Label input;
+        private System.Windows.Forms.Label output;
     }
 }
 
--- FolderCrawler/Form1.cs
+++ FolderCrawler/Form1.cs
@@ -16,5 +16,20 @@ namespace FolderCrawler
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
